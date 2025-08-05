be56c9b6707ccfdbdd55d3915d7ff9076060a5fb
Amit Verma <vamit20000@gmail.com>
2025-08-05 20:58:56 +0530

    question card partially done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
@@ -9,8 +10,24 @@ interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
+
+const filterQuestion = [
+  {
+    _id: "1",
+    title: "How to use Next.js?",
+    description: "A beginner's guide to Next.js.",
+    tags: ["nextjs", "react", "javascript"],
+  },
+  {
+    _id: "2",
+    title: "What is React?",
+    description: "Understanding the basics of React.",
+    tags: ["react", "javascript", "frontend"],
+  }
+]
+
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query="",filter='' } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -29,10 +46,10 @@ const Home = async ({ searchParams }: SearchParams) => {
       </section>
       <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
+        {/* //TODO: Fetch and map through questions based on searchParams */}
+        {filterQuestion.map((question) => (
+          <QuestionCard key={QuestionCard._id} question={question} />
+        ))}
       </div>
     </>
   );
